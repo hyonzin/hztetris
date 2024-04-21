abba9f57138b1634e3cb9b4080571eb14fa278e0
hyonzin <hyeonjin507@gmail.com>
2024-04-21 18:14:03 +0900

    deploy as compose

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker stop hztetris-client-app 2> /dev/null
-sleep 1
+# docker stop hztetris-client-app 2> /dev/null
+# sleep 1
+# docker run --rm -d -p 8000:8000 --name hztetris-client-app hztetris-client:latest
 
-docker run --rm -d -p 8000:8000 --name hztetris-client-app hztetris-client:latest
-
+docker compose up -d
