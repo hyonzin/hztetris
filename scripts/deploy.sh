4c1fcced725e2e6879e0573023df523ee360d74c
hyonzin <hyeonjin507@gmail.com>
2024-04-23 22:59:34 +0900

    touch env file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,8 @@
 # sleep 1
 # docker run --rm -d -p 8000:8000 --name hztetris-client-app hztetris-client:latest
 
-docker compose --env-file .env.local up -d
+ENV_FILE=.env.local
+touch $ENV_FILE
+
+docker compose --env-file $ENV_FILE up -d
+
